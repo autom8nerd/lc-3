9f79a521a042f4526da9c9a0b3348eb3028d1598
iamancil <24bcs015@nith.ac.in>
2026-05-11 20:56:45 +0530

    yet to write the operations

--- vm.c
+++ vm.c
@@ -61,3 +61,54 @@ enum {
   FL_ZRO = 1 << 1, // Z 010
   FL_NEG = 1 << 2  // N 100
 };
+
+int main(int argc, const char *argv[]) {
+
+  reg[R_COND] = FL_ZRO;
+  // pc start address at 0x4600
+  enum { PC_START = 0x4600 };
+  reg[R_PC] = PC_START;
+
+  int going = 1;
+  while (going) {
+    uint16_t instr = mem_read(reg[R_PC]++);
+    uint16_t op =
+        instr >>
+        12; // op contains the opcode; the upper 4 bits out of the 16 bits
+    switch (op) {
+    case OP_ADD:
+      @{ ADD } break;
+    case OP_AND:
+      @{ AND } break;
+    case OP_NOT:
+      @{ NOT } break;
+    case OP_BR:
+      @{ BR } break;
+    case OP_JMP:
+      @{ JMP } break;
+    case OP_JSR:
+      @{ JSR } break;
+    case OP_LD:
+      @{ LD } break;
+    case OP_LDI:
+      @{ LDI } break;
+    case OP_LDR:
+      @{ LDR } break;
+    case OP_LEA:
+      @{ LEA } break;
+    case OP_ST:
+      @{ ST } break;
+    case OP_STI:
+      @{ STI } break;
+    case OP_STR:
+      @{ STR } break;
+    case OP_TRAP:
+      @{ TRAP } break;
+    case OP_RES:
+    case OP_RTI:
+    default:
+      @{ BAD OPCODE } break;
+    }
+  }
+}
+}
