3fbb62c402a703c6af27979522f078a4ad644d97
iamancil <24bcs015@nith.ac.in>
2026-06-02 20:11:56 +0530

    added OP_NOT

--- vm.c
+++ vm.c
@@ -108,8 +108,15 @@ int main(int argc, const char *argv[]) {
         update_flags(r0);
       }
     } break;
-    case OP_NOT:
-      @{ NOT } break;
+    case OP_NOT: {
+      {
+        uint16_t r0 = (instr >> 9) & 0x7;
+        uint16_t r1 = (instr >> 6) & 0x7;
+
+        reg[r0] = ~reg[r1];
+        update_flags(r0);
+      }
+    } break;
     case OP_BR:
       @{ BR } break;
     case OP_JMP:
